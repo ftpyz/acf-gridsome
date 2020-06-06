08e4be325390ab2d8ee082f7028d8eecd5916b10
Fuat POYRAZ <fuat@fuatpoyraz.com>
2020-06-06 19:29:07 +0300

    add array support

--- index.php
+++ index.php
@@ -7,12 +7,21 @@
  * Version: 0.5
  * Plugin URI: https://github.com/ftpyz/acf-gridsome
  */
+
 add_filter( 'acf/format_value', function ( $value ) {
   if ( $value instanceof WP_Post ) {
     return [
       'post_type' => $value->post_type,
       'id'        => $value->ID,
     ];
+  }
+
+  return $value;
+}, 100 );
+
+add_filter( 'acf/format_value/type=select', function ( $value ) {
+  if(count($value)==0){
+    return false;
   }
 
   return $value;
